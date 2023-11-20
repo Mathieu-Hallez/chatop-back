3e0924d954e49801981fa76162640912085ae6d3
Mr_mickcraft 45 <mickcraft45@gmail.com>
2023-11-20 11:50:25 +0100

    Init Commit and configurate project

---
+++ src/main/java/com/chatop/chatopback/ChatopBackApplication.java
@@ -0,0 +1,13 @@
+package com.chatop.chatopback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatopBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatopBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chatop/chatopback/ChatopBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatop.chatopback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatopBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
