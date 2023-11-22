845c3020075b315e5684b0ff95346a8f96f83913
Mr_mickcraft 45 <mickcraft45@gmail.com>
2023-11-22 17:34:35 +0100

    Test AuthController.java

--- src/main/java/com/chatop/chatopback/controllers/AuthController.java
+++ src/main/java/com/chatop/chatopback/controllers/AuthController.java
@@ -1,8 +1,13 @@
 package com.chatop.chatopback.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
 
+    @GetMapping("/")
+    public String getRessource() {
+        return "a value...";
+    }
 }
