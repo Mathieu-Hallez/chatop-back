6a629ffdfbc0f0a399d7cd247b6336f454a22fa4
Mr_mickcraft 45 <mickcraft45@gmail.com>
2023-12-02 22:14:22 +0100

    Add @RequestMapping for controllers

--- src/main/java/com/chatop/chatopback/controllers/AuthController.java
+++ src/main/java/com/chatop/chatopback/controllers/AuthController.java
@@ -7,14 +7,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/auth")
 public class AuthController {
 
     private static final Logger log = LoggerFactory.getLogger(AuthController.class);
