1107e3c1b23408d66bf2de9c387bff3eb324f96d
Mr_mickcraft 45 <mickcraft45@gmail.com>
2023-12-04 20:05:33 +0100

    Format Time of Dto Object

--- src/main/java/com/chatop/chatopback/payload/rental/RentalDto.java
+++ src/main/java/com/chatop/chatopback/payload/rental/RentalDto.java
@@ -5,9 +5,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 @Data
 public class RentalDto extends CreateRentalDto {
+
     private Long id;
     @JsonAlias("owner_id")
     @JsonProperty("owner_id")
@@ -18,5 +23,15 @@ public class RentalDto extends CreateRentalDto {
     @JsonAlias("updated_at")
     @JsonProperty("updated_at")
     private String updatedAt;
+
+    public void setCreatedAt(String date) {
+        String PATTERN_FORMAT = "yyyy/MM/dd";
+        this.createdAt = DateTimeFormatter.ofPattern(PATTERN_FORMAT).withZone( ZoneId.systemDefault() ).format(Timestamp.valueOf(date).toLocalDateTime());
+    }
+
+    public void setUpdatedAt(String date) {
+        String PATTERN_FORMAT = "yyyy/MM/dd";
+        this.updatedAt = DateTimeFormatter.ofPattern(PATTERN_FORMAT).withZone( ZoneId.systemDefault() ).format(Timestamp.valueOf(date).toLocalDateTime());
+    }
 
 }
