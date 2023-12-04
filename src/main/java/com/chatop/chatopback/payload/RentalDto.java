af10f3364f50e3a5d445008377236f30102e0a1a
Mr_mickcraft 45 <mickcraft45@gmail.com>
2023-12-04 15:31:46 +0100

    Edit Rental Dto

--- src/main/java/com/chatop/chatopback/payload/RentalDto.java
+++ src/main/java/com/chatop/chatopback/payload/RentalDto.java
@@ -6,9 +6,14 @@ import java.math.BigDecimal;
 
 @Data
 public class RentalDto {
+    private Long id;
     private String name;
     private BigDecimal surface;
     private BigDecimal price;
     private String picture;
     private String description;
+    private Long owner_id;
+    private String created_at;
+    private String updated_at;
+
 }
